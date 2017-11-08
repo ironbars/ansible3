20db8aa068cca057ac17d6da3444578c94296539
Marc J. Patton <pattonmj@umich.edu>
2017-11-08 14:20:26 -0500

    Changed some automation order

--- ansible-build.sh
+++ ansible-build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-systemctl disable ansible-build.service
-
-ansible-playbook /etc/ansible/site.yml
-
 sed -ie 's/^#en_US/en_US/g' /etc/locale.gen && rm /etc/locale.gene
-
 locale-gen
 localectl set-locale LANG=en_US.UTF-8
 hwclock --systohc --utc
@@ -13,3 +8,4 @@ timedatectl set-timezone "America/Detroit"
 sed -ie 's/^#NTP=/NTP=pool\.ntp\.org/g' /etc/systemd/timesyncd.conf && rm /etc/systemd/timesyncd.confe
 timedatectl set-ntp true
 
+ansible-playbook /etc/ansible/site.yml
--- ansible-start.sh
+++ ansible-start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 ansible-playbook /etc/ansible/site.yml -t 'network_setup'
-cp /etc/ansible/ansible-build.service /etc/systemd/system/
 chmod +x ansible-build.sh
-systemctl enable ansible-build.service
 exit

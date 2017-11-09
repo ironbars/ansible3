d02621da8d841f8f05b4a05dbe4d2aeb042a1702
Marc J. Patton <pattonmj@umich.edu>
2017-11-08 22:56:48 -0500

    Updated config-start script

--- config-start.sh
+++ config-start.sh
@@ -5,9 +5,13 @@ host=$1
 if [ -z $host ]; then
 	echo "Provide hostname (i.e $0 <hostname>)"
 	exit 1
+fi
 
 # Set hostname
 echo $host > /etc/hostname
+
+# Get root device for later
+root_dev=$(mount | awk '$3 ~ /^\/$/ {print $1}')
 
 # Install bootloader
 bootctl install
@@ -18,16 +22,20 @@ default    arch
 timeout    5
 editor     0
 EOF
+echo "Bootloader config file created"
 
 cat << EOF > /boot/loader/entries/arch.conf
 title        Arch Linux
 linux        /vmlinuz-linux
 initrd       /initramfs-linux.img
-options      root=PARTUUID=$(cat /etc/fstab | grep -v "#" | awk '$2 ~ /^\/$/ {print $1}' | cut -f2 -d"=") rw
+options      root=PARTUUID=$(blkid -s PARTUUID -o value $root_dev) rw
 EOF
+echo "Bootlaoder entry created"
 
 # Prepare post-reboot config script
 chmod +x /etc/ansible/config.sh
 
 # Perform network setup via Ansible
 ansible-playbook /etc/ansible/site.yml -t 'network_setup'
+
+exit
